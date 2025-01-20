5fb3c907636539936e115e64f9b2017edd4e73f0
Kieeran <21521856@gm.uit.edu.vn>
2025-01-20 22:17:50 +0700

    Add the "spawnObject" function to spawn individual objects and the "spawnObjects" function to spawn all objects.

--- Assets/Scripts/SpawnObjects.cs
+++ Assets/Scripts/SpawnObjects.cs
@@ -22,30 +22,60 @@ public class SpawnObjects : MonoBehaviour
 
     void Start()
     {
-        for (int i = 0; i < knifeAngles.Count; i++)
-        {
-            GameObject knife = Instantiate(knifePrefab, objectHolder);
+        spawnObjects();
 
-            Vector2 pos = new Vector2(
-                (float)(objectHolder.position.x + radius * Math.Cos(degToRad(knifeAngles[i]))),
-                (float)(objectHolder.position.y + radius * Math.Cos(degToRad(knifeAngles[i])))
-            );
+        // for (int i = 0; i < knifeAngles.Count; i++)
+        // {
+        //     GameObject knife = Instantiate(knifePrefab, objectHolder);
 
-            knife.transform.position = pos;
+        //     Vector2 pos = new Vector2(
+        //         (float)(objectHolder.position.x + radius * Math.Cos(degToRad(knifeAngles[i]))),
+        //         (float)(objectHolder.position.y + radius * Math.Sin(degToRad(knifeAngles[i])))
+        //     );
 
-            float dx = objectHolder.position.x - knife.transform.position.x;
-            float dy = objectHolder.position.y - knife.transform.position.y;
+        //     knife.transform.position = pos;
 
-            float angleRad = (float)Math.Atan2(-dx, dy);
+        //     float dx = objectHolder.position.x - knife.transform.position.x;
+        //     float dy = objectHolder.position.y - knife.transform.position.y;
 
-            float angleDeg = (float)(angleRad * (180 / Math.PI));
+        //     float angleRad = (float)Math.Atan2(-dx, dy);
 
-            knife.transform.Rotate(0, 0, angleDeg);
-        }
+        //     float angleDeg = (float)(angleRad * (180 / Math.PI));
+
+        //     knife.transform.Rotate(0, 0, angleDeg);
+        // }
     }
 
     private float degToRad(float deg)
     {
         return (float)(deg * (Math.PI / 180f));
+    }
+
+    private void spawnObjects()
+    {
+        this.spawnObject(knifeAngles, knifePrefab);
+        this.spawnObject(appleAngles, applePrefab);
+        this.spawnObject(goldenAppleAngles, goldenApplePrefab);
+    }
+
+    private void spawnObject(List<float> anglesArray, GameObject prefabObject)
+    {
+        for (int i = 0; i < anglesArray.Count; i++)
+        {
+            GameObject obj = Instantiate(prefabObject, objectHolder);
+
+            float tempRadius = this.radius;
+            // if (obj.name.Contains("knife"))
+            // {
+            //     tempRadius -= 60;
+            // }
+
+            Vector2 pos = new Vector2(
+                (float)(objectHolder.position.x + tempRadius * Math.Cos(degToRad(anglesArray[i]))),
+                (float)(objectHolder.position.y + tempRadius * Math.Cos(degToRad(anglesArray[i])))
+            );
+
+            obj.transform.position = pos;
+        }
     }
 }
