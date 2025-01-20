601a4cf0f46c86d9a045d98b41ddc3493ff33751
Kieeran <21521856@gm.uit.edu.vn>
2025-01-20 21:30:30 +0700

    Align knives

--- Assets/Scripts/SpawnObjects.cs
+++ Assets/Scripts/SpawnObjects.cs
@@ -18,16 +18,28 @@ public class SpawnObjects : MonoBehaviour
     {
         for (int i = 0; i < knifeAngles.Count; i++)
         {
-            GameObject knife = Instantiate(knifePrefab);
-            knife.transform.SetParent(objectHolder);
+            GameObject knife = Instantiate(knifePrefab, objectHolder);
 
             Vector2 pos = new Vector2(
-                (float)(objectHolder.position.x + radius * Math.Cos(knifeAngles[i])),
-                (float)(objectHolder.position.y + radius * Math.Cos(knifeAngles[i]))
+                (float)(objectHolder.position.x + radius * Math.Cos(degToRad(knifeAngles[i]))),
+                (float)(objectHolder.position.y + radius * Math.Cos(degToRad(knifeAngles[i])))
             );
 
             knife.transform.position = pos;
-            knife.transform.localScale = Vector3.one;
+
+            float dx = objectHolder.position.x - knife.transform.position.x;
+            float dy = objectHolder.position.y - knife.transform.position.y;
+
+            float angleRad = (float)Math.Atan2(-dx, dy);
+
+            float angleDeg = (float)(angleRad * (180 / Math.PI));
+
+            knife.transform.Rotate(0, 0, angleDeg);
         }
+    }
+
+    private float degToRad(float deg)
+    {
+        return (float)(deg * (Math.PI / 180f));
     }
 }
