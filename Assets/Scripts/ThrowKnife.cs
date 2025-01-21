df6245d81a10101cf3698615fb41212db28c5932
Kieeran <21521856@gm.uit.edu.vn>
2025-01-21 10:31:34 +0700

    Add a collider to the knife. Create and throw the knif

--- Assets/Scripts/ThrowKnife.cs
+++ Assets/Scripts/ThrowKnife.cs
@@ -6,12 +6,34 @@ using UnityEngine.Events;
 public class ThrowKnife : MonoBehaviour
 {
     [SerializeField] private GameObject knifePrefab;
+    [SerializeField] private float force;
 
-    void Update()
+    private GameObject currentKnife;
+    private void Start()
+    {
+        CreateNewKnife();
+    }
+
+    private void CreateNewKnife()
+    {
+        currentKnife = Instantiate(knifePrefab, transform);
+    }
+
+    private void Update()
     {
         if (Input.touchCount > 0 || Input.GetMouseButtonDown(0))
         {
             Debug.Log("Touch the screen");
+
+            Rigidbody2D rigidbody2D = currentKnife.AddComponent<Rigidbody2D>();
+
+            rigidbody2D.gravityScale = 0;
+
+            rigidbody2D.AddForce(Vector2.up * force * Time.deltaTime, ForceMode2D.Impulse);
+
+            Destroy(currentKnife, 1f);
+
+            CreateNewKnife();
         }
     }
 }
