d5ce431fe13f89bea743400a3558d547b2e01ec5
Kieeran <21521856@gm.uit.edu.vn>
2025-01-21 23:20:51 +0700

    Add the necessary variables for the target's ScriptableObject

--- Assets/Scripts/Scriptable Object/Target/TargetConfig.cs
+++ Assets/Scripts/Scriptable Object/Target/TargetConfig.cs
@@ -5,5 +5,11 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Configs/Target/TargetConfig", fileName = "TargetConfig")]
 public class TargetConfig : ScriptableObject
 {
+    [field: SerializeField] public bool isTargetRotatingContinuously { get; private set; }
+    [field: SerializeField] public bool isReverve { get; private set; }
 
+    [field: SerializeField] public float initAngularVel { get; private set; }
+    [field: SerializeField] public float rotationSpeed { get; private set; }
+    [field: SerializeField] public float rotationDuration { get; private set; }
+    [field: SerializeField] public float anglDamping { get; private set; }
 }
