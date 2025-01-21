0cba1a8b85da28f93b3c33ffe45425f2ddce22af
Kieeran <21521856@gm.uit.edu.vn>
2025-01-21 23:14:20 +0700

    Create scriptable object for apple and knife

---
+++ Assets/Scripts/Scriptable Object/Apple/AppleConfig.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Configs/Apple/AppleConfig", fileName = "AppleConfig")]
+public class AppleConfig : ScriptableObject
+{
+
+}
---
+++ Assets/Scripts/Scriptable Object/Knife/KnifeConfig.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(menuName = "Configs/Knife/KnifeConfig", fileName = "KnifeConfig")]
+public class KnifeConfig : ScriptableObject
+{
+
+}
