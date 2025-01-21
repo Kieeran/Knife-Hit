d615dbf8906f662f22487e792f2d422d9382a97a
Kieeran <21521856@gm.uit.edu.vn>
2025-01-22 01:45:43 +0700

    Create pool for "spawn knife" and "throw knife"

--- Assets/Scripts/KnifeManager.cs
+++ Assets/Scripts/KnifeManager.cs
@@ -15,4 +15,47 @@ public class KnifeManager : MonoBehaviour
 
         Instance = this;
     }
+
+    private Queue<GameObject> spawnKnives;
+    private Queue<GameObject> throwKnives;
+
+    [SerializeField] private GameObject spawnKnifePrefab;
+    [SerializeField] private GameObject throwKnifePrefab;
+    private int amount;
+
+
+    [SerializeField] private List<KnifeConfig> knifeConfigs;
+
+    private void Start()
+    {
+        amount = 10;
+
+        InitPooling();
+    }
+
+    private void InitPooling()
+    {
+        CreateSpawnKnives(amount);
+        CreateThrowKnives(amount);
+    }
+
+    private void CreateSpawnKnives(int amount)
+    {
+        for (int i = 0; i < amount; i++)
+        {
+            GameObject spawnKnife = Instantiate(spawnKnifePrefab);
+            spawnKnife.gameObject.SetActive(false);
+            spawnKnives.Enqueue(spawnKnife);
+        }
+    }
+
+    private void CreateThrowKnives(int amount)
+    {
+        for (int i = 0; i < amount; i++)
+        {
+            GameObject throwKnife = Instantiate(throwKnifePrefab);
+            throwKnife.gameObject.SetActive(false);
+            throwKnives.Enqueue(throwKnife);
+        }
+    }
 }
