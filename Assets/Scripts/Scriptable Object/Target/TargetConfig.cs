fb75d946a09876c5fa5396b3d38e05d286431e33
Kieeran <21521856@gm.uit.edu.vn>
2025-01-21 23:16:53 +0700

    Create scriptable object for target

---
+++ Assets/Scripts/Scriptable Object/Target/TargetConfig.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Configs/Target/TargetConfig", fileName = "TargetConfig")]
+public class TargetConfig : ScriptableObject
+{
+
+}
