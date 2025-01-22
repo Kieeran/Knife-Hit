cef2df94c310d94fb0bb3b925081a5d7e9abecb3
Kieeran <21521856@gm.uit.edu.vn>
2025-01-22 09:08:11 +0700

    Create apple configs for apple and golden apple

--- Assets/Scripts/Scriptable Object/Apple/AppleConfig.cs
+++ Assets/Scripts/Scriptable Object/Apple/AppleConfig.cs
@@ -5,5 +5,6 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Configs/Apple/AppleConfig", fileName = "AppleConfig")]
 public class AppleConfig : ScriptableObject
 {
-
+    [field: SerializeField] public int appleID { get; private set; }
+    [field: SerializeField] public Sprite appleSprite { get; private set; }
 }
