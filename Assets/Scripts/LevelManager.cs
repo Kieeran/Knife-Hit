e5700b53de25af0e54f51fc30b533edc1425724e
Kieeran <21521856@gm.uit.edu.vn>
2025-01-22 21:49:02 +0700

    Set up a singleton for LevelManager

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -13,10 +13,17 @@ public class LevelData
 
 public class LevelManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    public static LevelManager Instance { get; private set; }
 
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
     }
 
     // Update is called once per frame
