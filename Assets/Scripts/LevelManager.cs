d53c620c0276000ae6e59b2bb4f27c926c1361bd
Kieeran <21521856@gm.uit.edu.vn>
2025-01-22 21:40:34 +0700

    Create a new class LevelData

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -2,17 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class LevelData
+{
+    public List<StageConfig> stageConfigs;
+    public List<StageConfig> stageBossConfigs;
+    public List<TargetConfig> targetConfigs;
+    public int stageCount = 5;
+}
+
 public class LevelManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
