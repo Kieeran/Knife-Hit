0606a7440c305e3b10d82fe8eabaa699f15379d4
Kieeran <21521856@gm.uit.edu.vn>
2025-01-23 11:42:34 +0700

    Create the necessary functions for doing the "KnifeAmountBar".

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
     public static UIManager Instance { get; private set; }
+    [SerializeField] private Transform knifeAmountBar;
+    [SerializeField] private GameObject knifeIconPrefab;
     private void Awake()
     {
         if (Instance != null && Instance != this)
@@ -14,5 +17,37 @@ public class UIManager : MonoBehaviour
         }
 
         Instance = this;
+    }
+
+    public void SetupKnifeAmountBar(int amount)
+    {
+        foreach (Transform child in knifeAmountBar)
+        {
+            if (child.gameObject.activeSelf == true)
+            {
+                Destroy(child);
+            }
+        }
+
+        for (int i = 0; i < amount; i++)
+        {
+            Instantiate(knifeIconPrefab, knifeAmountBar);
+        }
+    }
+
+    public void RemoveKnife()
+    {
+        for (int i = knifeAmountBar.childCount - 1; i >= 0; i--)
+        {
+            Transform child = knifeAmountBar.GetChild(i);
+
+            Image childImage = child.GetComponent<Image>();
+
+            if (childImage != null && childImage.color != Color.black)
+            {
+                childImage.color = Color.black;
+                return;
+            }
+        }
     }
 }
